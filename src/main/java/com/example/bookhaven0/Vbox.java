48a315b0bb1b5b2d19579ed91ab0bb98bf67dd7b
NathanWilson03 <113388726+NathanWilson03@users.noreply.github.com>
2024-04-21 18:13:57 -0400

    Delete src/main directory

--- src/main/java/com/example/bookhaven0/Vbox.java
+++
@@ -1,4 +0,0 @@
-package com.example.bookhaven0;
-
-public class Vbox {
-}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.bookhaven0 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.bookhaven0 to javafx.fxml;
-    exports com.example.bookhaven0;
-}
