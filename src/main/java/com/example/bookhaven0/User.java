089b2c825fde7d31b82156eb22b620959f5d0737
NathanWilson03 <113388726+NathanWilson03@users.noreply.github.com>
2024-04-21 18:13:07 -0400

    Add files via upload

---
+++ src/main/java/com/example/bookhaven0/User.java
@@ -0,0 +1,43 @@
+package com.example.bookhaven0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private String userName;
+    private String password;
+    private List<String> bookCollection = new ArrayList<>();
+
+
+
+    public User(String name, String key, List<String> collection) {
+        userName = name;
+        password = key;
+
+
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return userName + "\n" ; //toString
+    }
+
+
+}
---
+++ src/main/java/com/example/bookhaven0/Vbox.java
@@ -0,0 +1,4 @@
+package com.example.bookhaven0;
+
+public class Vbox {
+}
